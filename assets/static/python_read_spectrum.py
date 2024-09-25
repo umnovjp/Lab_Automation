cd27ca7a9faa640395706b8c2013f49aff1738e9
umnovjp <umnovjp@yahoo.com>
2024-09-24 23:38:57 -0500

    created 3 python scripts one more started

---
+++ assets/static/python_read_spectrum.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 20:44:53 2024
+
+save spectrum diagram 
+
+@author: Remote
+"""
+
+import pyvisa as visa
+rm=visa.ResourceManager()
+rm.list_resources()
+Instr19=rm.open_resource('GPIB0::19::INSTR')
+Usb0=rm.open_resource('USB0::0x0B5B::0xFFF9::815093_146_11::INSTR')
+Instr19.write('freq 2.355000000000E+09')
+power=Usb0.query("trace:data?")
+import numpy as np
+import matplotlib.pyplot as plt
+power_list=[]
+power_split=power.split(',')
+power_split.pop(0)
+power_split.pop(550)
+for x in range(len(power_split)): 
+    power_list.append(float(power_split[x]))
+plt.plot(power_list)
+plt.show()
+Instr19.close()
+Usb0.close()
---
+++ assets/static/python_set_start_freq.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 20:38:16 2024
+
+@author: Remote
+"""
+
+import pyvisa as visa
+rm=visa.ResourceManager()
+rm.list_resources()
+Usb0=rm.open_resource('USB0::0x0B5B::0xFFF9::815093_146_11::INSTR')
+Usb0.write('frequency:start 2300000000')
+Usb0.close()
