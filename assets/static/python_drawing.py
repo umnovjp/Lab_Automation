471678bc1f4970dddf2df7e135c85f13baf294c8
umnovjp <umnovjp@yahoo.com>
2024-12-05 23:44:06 -0600

    finally ipywidgets started working but only in jupyter lab and not in spyder

--- assets/static/python_drawing.py
+++ assets/static/python_drawing.py
@@ -2,13 +2,18 @@
 """
 Spyder Editor
 
-Scripts plots log plot of tek0034.csv file located in the same folder
+power_split2, power_split is a list
+power_array is numpy.ndarray
+power is str
+plt.semilogy(w1,s1)
+length of power_split2 is 1 but length of power_split is 551
 
-This is a temporary script file."""
+This is a temporary script file.
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 w1, s1 = np.loadtxt("./tek0034.csv", skiprows=21, delimiter=",", unpack=True)
-plt.rcParams['agg.path.chunksize']=150
-plt.semilogy(w1,s1)
+plt.rcParams['agg.path.chunksize']=500
+plt.plot(w1,s1)
