6eb844df33f0d77c00775657857535a727064637
umnovjp <umnovjp@yahoo.com>
2024-12-06 23:16:58 -0600

    adding more options to scripts

--- assets/static/python_drawing.py
+++ assets/static/python_drawing.py
@@ -16,4 +16,6 @@ import matplotlib.pyplot as plt
 
 w1, s1 = np.loadtxt("./tek0034.csv", skiprows=21, delimiter=",", unpack=True)
 plt.rcParams['agg.path.chunksize']=500
+plt.xlim(-0.005,0.005)
+plt.ylim(-1,1)
 plt.plot(w1,s1)
